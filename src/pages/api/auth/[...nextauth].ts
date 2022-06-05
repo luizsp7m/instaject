213eb57120ec7fffc745c25c82991cf05a197143
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-06-05 19:52:34 -0300

    env: Alterações nas variáveis

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,10 @@ import GithubProvider from "next-auth/providers/github";
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+
+  secret: process.env.NEXTAUTH_SECRET,
 })
